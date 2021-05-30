1a0f44d88088c42ef00aea25797df613b3dfe217
harikuttan01 <harikuttan01@users.noreply.github.com>
2021-05-30 15:04:24 +0530

    Added a function called wallis and monte_carlo

--- estimate.py
+++ estimate.py
@@ -1,16 +1,38 @@
 import math
 import unittest
+import random
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
-    def test_high_iters(self):
-        for i in range(500, 600):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+
+def wallis(n):
+    p = 1
+    for i in range(1, n):
+        p = p*4*math.pow(i, 2)/(4*math.pow(i, 2)-1)
+    return 2*p
+
+
+def monte_carlo(n):
+    incircle = 0
+    outcircle = 0
+    for i in range(n):
+        x, y = (random.random(), random.random())
+        dist = math.sqrt(math.pow(x, 2)+math.pow(y, 2))
+        if dist < 1:
+            incircle = incircle + 1
+        else:
+            outcircle = outcircle+1
+
+    p = 4*(incircle/n)
+    return(p)
+
+def test_high_iters(self):
+    for i in range(500, 600):
+        pi = wallis(i)
+        self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
 
 
 class TestMC(unittest.TestCase):
